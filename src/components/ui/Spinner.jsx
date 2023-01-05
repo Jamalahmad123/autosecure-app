fc55851467cbb75cabf4960e451f65f3f6f62b0c
jamal ahmad <jamalcs007@gmail.com>
2023-01-05 21:51:56 +0500

    page loader bug fixed

--- src/components/ui/Spinner.jsx
+++ src/components/ui/Spinner.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 const Spinner = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+  }, []);
+
   return (
     <div className="max-h-screen min-h-screen flex items-center justify-center bg-clrLightGray z-[9999]">
       <span className="loader"></span>
